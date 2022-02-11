6c029a111f1dcb136c272c30c69c6b94b3af7d00
Madhushreevp <46991711+Madhushreevp@users.noreply.github.com>
2022-02-11 10:18:55 +0530

    Add files via upload

---
+++ 3_Implementation/header.h
@@ -0,0 +1,11 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<math.h>
+#include<string.h>
+void caesar_decrypt();
+void caesar_encrypt();
+void rsa_encrypt();
+void rsa_decrypt();
+long int findGCD(long int,long int);
+long int multipicative_inverse(long int,long int);
+void key_pair(long int,long int,long int);
