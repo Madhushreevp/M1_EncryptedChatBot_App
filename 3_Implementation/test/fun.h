53f43d98a66e15dfda762c0c776928b64d5f0817
Madhushreevp <46991711+Madhushreevp@users.noreply.github.com>
2022-02-11 15:06:22 +0530

    Add files via upload

---
+++ 3_Implementation/test/fun.h
@@ -0,0 +1,13 @@
+/*
+**
+ * @file fun.h
+ * @brief 
+ * 
+ */
+void caesar_decrypt();
+void caesar_encrypt();
+void rsa_encrypt();
+void rsa_decrypt();
+long int findGCD(long int,long int);
+long int multipicative_inverse(long int,long int);
+void key_pair(long int,long int,long int);
